65ac775ef98d01f220264dd2fcde38a0d511d81c
KuzcoDev <kuzco.dev@proton.me>
2025-05-22 10:28:16 +0200

    feat: user & admin route layout

---
+++ app/(user)/layout.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link";
+
+export default function UserLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div>
+      <main className="flex-grow">{children}</main>
+    </div>
+  );
+}
---
+++ app/(user)/user/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Hello User</h1>
+    </div>
+  );
+}
